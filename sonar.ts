ad1408d23c00bfc66b85852a6bf6f2a0ad21f694
xwsheng <xwsheng@msn.com>
2019-08-31 02:51:40 +0000

    修复错误

--- sonar.ts
+++ sonar.ts
@@ -10,7 +10,7 @@ enum PingUnit {
 /**
  * Sonar and ping utilities
  */
-//% color="#2c3e50" weight=10
+//% color="#2c3e50" weight=10 block="器件扩展"
 namespace sonar {
     /**
      * Send a ping and get the echo time (in microseconds) as a result
