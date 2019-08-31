7aa8c8bfc90f28526fcd476679b79db32d76ca3c
xwsheng <xwsheng@msn.com>
2019-08-31 04:02:50 +0000

    Repair Error!

--- sonar.ts
+++ sonar.ts
@@ -20,7 +20,7 @@ namespace microbit_extend{
      * @param maxCmDistance maximum distance in centimeters (default is 500)
      */
     //% blockId=sonar_ping block="ping trig %trig|echo %echo|unit %unit"
-    export function ping(trig: DigitalPin, echo: DigitalPin, unit: PingUnit, maxCmDistance = 500): number {
+    export function sonar_ping(trig: DigitalPin, echo: DigitalPin, unit: PingUnit, maxCmDistance = 500): number {
         // send pulse
         pins.setPull(trig, PinPullMode.PullNone);
         pins.digitalWritePin(trig, 0);
